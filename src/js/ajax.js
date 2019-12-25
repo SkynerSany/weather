4306ac037d18df56d7ce63481075673a8b5196c9
SkynerSany <skynerstudent@gmail.com>
2019-12-25 21:46:45 +0300

    fix: ajax request

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -16,7 +16,7 @@ function ajax(url) {
 
 const obj = {
   getLocation: () => ajax('https://ipinfo.io/json?token=b146feb3614af8'),
-  getWeather: (location, state, language, temperature) => ajax(`http://api.openweathermap.org/data/2.5/forecast?q=${location},${state}&lang=${language}&units=${temperature}&APPID=6ebdd0fd3d344c146430ede697227fdf`),
+  getWeather: (location, state, language, temperature) => ajax(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=${location},${state}&lang=${language}&units=${temperature}&APPID=6ebdd0fd3d344c146430ede697227fdf`),
   getBackground: (town) => ajax(`https://api.unsplash.com/photos/random?query=town,${town}&client_id=07b8adbc371366a8a45043f03b4b409735ce0ad155a68e10c8dfcddeaa261b78`),
   getNewLocation: (town) => ajax(`https://api.opencagedata.com/geocode/v1/json?q=${town}&key=a7f2d1bad2c34af5ae3327dbcc36bd1b`),
 };
